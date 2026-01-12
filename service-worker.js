26d5f556955df2132388b5cbda618f308970237b
ymp-engr-dept <ymp.engr.dept@gmail.com>
2026-01-12 23:29:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // ▼▼▼ ここを更新するたびに書き換える（v1 -> v2 -> v3...） ▼▼▼
-const CACHE_NAME = 'suiren-audio-v0.4.0';
+const CACHE_NAME = 'suiren-audio-v0.4.10';
 // ▲▲▲ これが変わると、ブラウザは「更新がある」と判断します ▲▲▲
 
 const ASSETS_TO_CACHE = [
